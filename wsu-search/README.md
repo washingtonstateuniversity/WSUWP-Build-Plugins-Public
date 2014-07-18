2e88858aacc4e201669ea5f8c48f2b7ce6385922
jeremyfelt <jeremy.felt@gmail.com>
2014-07-18 16:21:16 -0700

    Update WSU Search to 0.2.0

--- wsu-search/README.md
+++ wsu-search/README.md
@@ -3,3 +3,29 @@
 Provides a connection to [WSU Search](https://github.com/washingtonstateuniversity/wsu-search/) from WordPress sites at WSU.
 
 Currently adds posts to WSU's `wsu-web` index as the global type of `page`.
+
+If `WSU_LOCAL_CONFIG` is defined as part of your development environment, th index will change to `wsu-local-dev`.
+
+## Document Structure
+
+The current document structure for pages saved by the WSU Search plugin is:
+
+```
+{
+	"body" : {
+		"title":               "Title of the page or post",
+		"date":                "2014-07-18 21:38:27",
+		"content":             "Content of the page or post",
+		"url":                 "http:\/\/wp.wsu.edu\/2014\/07\/18\/another-test-post\/",
+		"site_id":             52,
+		"hostname":            "news.wsu.dev",
+		"site_url":            "news.wsu.dev",
+		"network_id":          8,
+		"site_category":       [ "uncategorized" ],
+		"university_tag":      ["tag"],
+		"post_format":         [],
+		"university_category": [ "academic-subjects" ],
+		"university_location": [ "wsu-extension" ]
+	}
+}
+```
--- wsu-search/wsuwp-search.php
+++ wsu-search/wsuwp-search.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WSU Search
-Version: 0.0.1
+Version: 0.2.0
 Plugin URI: http://web.wsu.edu
 Description: Connects to Search
 Author: washingtonstateuniversity, jeremyfelt
@@ -22,24 +22,31 @@ class WSU_Search {
 	 * Setup hooks.
 	 */
 	public function __construct() {
-		add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
+		// Use a different index for pages saved during local development.
+		if ( defined( 'WSU_LOCAL_CONFIG' ) && true === WSU_LOCAL_CONFIG ) {
+			$this->index_api_url = 'http://134.121.140.161:9200/wsu-local-dev/page/';
+		}
+
+		add_action( 'transition_post_status', array( $this, 'save_post' ), 10, 3 );
+		add_action( 'transition_post_status', array( $this, 'delete_post' ), 10, 3 );
 	}
 
 	/**
 	 * When a post is saved, ensure that the most recent version is updated in the index. If this
 	 * does not yet exist in the index, then create the document and log the generated UUID.
 	 *
-	 * @param int     $post_id The ID of the post being saved.
-	 * @param WP_Post $post    The entire post object.
+	 * @param string  $new_status Post status being saved.
+	 * @param string  $old_status Previous post status.
+	 * @param WP_Post $post       The entire post object.
 	 *
 	 * @return null
 	 */
-	public function save_post( $post_id, $post ) {
+	public function save_post( $new_status, $old_status, $post ) {
 		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 			return NULL;
 		}
 
-		if ( in_array( $post->post_status, array( 'inherit', 'auto-draft' ) ) ) {
+		if ( 'publish' !== $new_status ) {
 			return NULL;
 		}
 
@@ -48,26 +55,58 @@ class WSU_Search {
 		// Data to be sent as JSON to Elasticsearch.
 		$data = array();
 
-		$search_id = get_post_meta( $post_id, '_wsusearch_doc_id', true );
+		$search_id = get_post_meta( $post->ID, '_wsusearch_doc_id', true );
 
 		// If this document already has an ID, we'll PUT to update it. If not, we'll POST a new document.
 		if ( $search_id ) {
 			$args['method'] = 'PUT';
-			$this->index_api_url .= sanitize_key( $search_id );
+			$this->index_api_url .= $this->_sanitize_es_id( $search_id );
 		} else {
 			$args['method'] = 'POST';
 		}
 
 		$data['title'] = $post->post_title;
 		$data['date'] = $post->post_date;
+		$data['modified'] = $post->post_modified;
 		$data['content'] = $post->post_content;
-		$data['url'] = get_permalink( $post_id );
+		$data['url'] = get_permalink( $post->ID );
+		$data['post_type'] = $post->post_type;
 
 		// Information about the site and network this came from.
 		$data['site_id'] = get_current_blog_id();
 
+		// Store the hostname - e.g. home.wsu.edu - as a field.
+		$home_url = parse_url( trailingslashit( get_home_url() ) );
+		$data['hostname'] = $home_url['host'];
+		$data['site_url'] = $home_url['host'];
+
+		// Only attach path if it isn't empty.
+		if ( '/' !== $home_url['path'] ) {
+			$data['site_url'] .= $home_url['path'];
+		}
+
 		if ( function_exists( 'wsuwp_get_current_network' ) ) {
-			$data['network_id'] = wsuwp_get_current_network()->id;
+			$data['network_id'] = intval( wsuwp_get_current_network()->id );
+		}
+
+		// Map each registered public taxonomy to the Elasticsearch document.
+		$taxonomies = get_taxonomies( array( 'public' => true ) );
+
+		foreach ( $taxonomies as $taxonomy ) {
+			$post_terms = wp_get_object_terms( $post->ID, $taxonomy, array( 'fields' => 'slugs' ) );
+			if ( ! is_wp_error( $post_terms ) ) {
+				if ( 'post_tag' === $taxonomy ) {
+					$data['university_tag'] = $post_terms;
+				} elseif( 'wsuwp_university_category' === $taxonomy ) {
+					$data['university_category'] = $post_terms;
+				} elseif( 'wsuwp_university_location' === $taxonomy ) {
+					$data['university_location'] = $post_terms;
+				} elseif( 'category' === $taxonomy ) {
+					$data['site_category'] = $post_terms;
+				} else {
+					$data[ $taxonomy ] = $post_terms;
+				}
+			}
 		}
 
 		$args['body'] = json_encode( $data );
@@ -77,8 +116,56 @@ class WSU_Search {
 
 		if ( ! empty( $response ) ) {
 			$response_data = json_decode( $response );
-			update_post_meta( $post_id, '_wsusearch_doc_id', sanitize_key( $response_data->_id ) );
+			update_post_meta( $post->ID, '_wsusearch_doc_id', $this->_sanitize_es_id( $response_data->_id ) );
 		}
+	}
+
+	/**
+	 * When a post is saved, delete it from the index if the end post status is something other
+	 * than 'publish'.
+	 *
+	 * @param string  $new_status Post status being saved.
+	 * @param string  $old_status Previous post status.
+	 * @param WP_Post $post       The entire post object.
+	 *
+	 * @return null
+	 */
+	public function delete_post( $new_status, $old_status, $post ) {
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return NULL;
+		}
+
+		// This document should be saving to a post status other than publish from a post status of publish.
+		if ( 'publish' === $new_status || 'publish' !== $old_status ) {
+			return NULL;
+		}
+
+		$search_id = get_post_meta( $post->ID, '_wsusearch_doc_id', true );
+
+		// This document has not yet been saved, no need to delete.
+		if ( false === $search_id ) {
+			return NULL;
+		}
+
+		$this->index_api_url .= $this->_sanitize_es_id( $search_id );
+
+		// Make a request to delete the existing document from Elasticsearch.
+		$response = wp_remote_request( $this->index_api_url, array( 'method' => 'DELETE' ) );
+
+		if ( ! is_wp_error( $response ) ) {
+			delete_post_meta( $post->ID, '_wsusearch_doc_id' );
+		}
+	}
+
+	/**
+	 * Sanitize the key returned from Elasticsearch. It should be a-z, A-Z, -, and _ only.
+	 *
+	 * @param string $id UUID returned from ES, or currently in use for a page.
+	 *
+	 * @return string sanitized string.
+	 */
+	private function _sanitize_es_id( $id ) {
+		return preg_replace( '/[^a-zA-Z0-9_\-]/', '', $id );
 	}
 }
 new WSU_Search();
