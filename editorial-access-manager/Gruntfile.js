070a189dcd8bc21f403e804ddc77bc209d27483f
jeremyfelt <jeremy.felt@gmail.com>
2014-11-14 14:15:16 -0800

    Update Editorial Access Manager 0.2.0

--- editorial-access-manager/Gruntfile.js
+++ editorial-access-manager/Gruntfile.js
@@ -3,9 +3,13 @@ module.exports = function ( grunt ) {
 		pkg : grunt.file.readJSON( 'package.json' ),
 		uglify : {
 			js : {
-				files : {
-					'build/js/post-admin.min.js' : ['js/post-admin.js']
-				}
+				files : [{
+					expand: true,
+					cwd: 'js',
+					src: '**/*.js',
+					dest: 'build/js/',
+					ext: '.min.js'
+				}]
 			}
 		},
 		jshint : {
@@ -17,6 +21,9 @@ module.exports = function ( grunt ) {
 			dist : {
 				files : {
 					'build/css/post-admin.css' : 'scss/post-admin.scss'
+				},
+				options : {
+					sourcemap: 'none'
 				}
 			}
 		},
@@ -32,6 +39,29 @@ module.exports = function ( grunt ) {
 				'scss/*'
 			],
 			tasks : ['jshint', 'uglify', 'sass', 'cssmin:backend']
+		},
+		makepot: {
+			target: {
+				options: {
+					domainPath: '/languages',
+					potFilename: 'editorial-access-manager.pot',
+					processPot: function( pot ) {
+						pot.headers['report-msgid-bugs-to'] = 'https://github.com/tlovett1/editorial-access-manager/issues\n';
+						pot.headers['plural-forms'] = 'nplurals=2; plural=n != 1;';
+						pot.headers['x-poedit-basepath'] = '.\n';
+						pot.headers['x-poedit-language'] = 'English\n';
+						pot.headers['x-poedit-country'] = 'United States\n';
+						pot.headers['x-poedit-sourcecharset'] = 'utf-8\n';
+						pot.headers['x-poedit-keywordslist'] = '__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c,_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n';
+						pot.headers['x-poedit-bookmarks'] = '\n';
+						pot.headers['x-poedit-searchpath-0'] = '.\n';
+						pot.headers['x-textdomain-support'] = 'yes\n';
+						return pot;
+					},
+					type: 'wp-plugin',
+					updateTimestamp: true
+				}
+			}
 		}
 	} );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
@@ -39,5 +69,6 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-sass' );
 	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.registerTask( 'default', ['jshint', 'uglify:js', 'sass', 'cssmin:backend'] );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.registerTask( 'default', ['jshint', 'uglify:js', 'sass', 'cssmin:backend', 'makepot'] );
 };
