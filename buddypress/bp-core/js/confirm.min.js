be61002f6caebbd5282a6e42bbb59b3416a04539
jeremyfelt <jeremy.felt@gmail.com>
2015-04-28 10:57:04 -0700

    Add BuddyPress 2.2.3.1

---
+++ buddypress/bp-core/js/confirm.min.js
@@ -0,0 +1,2 @@
+/*! buddypress - v2.2.3.1 - 2015-04-21 1:15:07 PM UTC - https://wordpress.org/plugins/buddypress/ */
+jQuery(document).ready(function(){jQuery("a.confirm").click(function(){return confirm(BP_Confirm.are_you_sure)?!0:!1})});
