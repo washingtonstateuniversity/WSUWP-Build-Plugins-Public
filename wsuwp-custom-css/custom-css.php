b5d03527d5d51ddd036ad26c135374603ca6e008
jeremyfelt <jeremy.felt@gmail.com>
2015-06-10 14:44:53 -0700

    Update WSUWP Custom CSS 2.1.4

--- wsuwp-custom-css/custom-css.php
+++ wsuwp-custom-css/custom-css.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: WSU Custom CSS
-Plugin URI: http://web.wsu.edu
+Plugin URI: https://web.wsu.edu/
 Description: Custom CSS via custom post type.
 Author: washingtonstateuniversity, jeremyfelt, automattic
-Version: 2.1.3
+Version: 2.1.4
 */
 
 /**
@@ -549,7 +549,7 @@ class WSU_Custom_CSS {
 		// We plan on the style being enqueued in the Spine parent theme. This should be considered temporary
 		// until we can rewrite to handle more than the Spine theme.
 		if ( 'spine' === $current_theme->template ) {
-			wp_enqueue_style( 'spine-custom-css', esc_url( $href ), array(), spine_get_script_version() );
+			wp_enqueue_style( 'spine-custom-css', $href, array(), spine_get_script_version() );
 		} else {
 			?>
 			<link rel="stylesheet" id="custom-css-css" type="text/css" href="<?php echo esc_url( $href ); ?>" />
