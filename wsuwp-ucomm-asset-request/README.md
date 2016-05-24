1d64330a7a50e09ce0e1f3ac51483e4edcb46564
Jeremy Felt <jeremy.felt@gmail.com>
2016-05-24 13:19:42 -0700

    Remove WSUWP UComm Asset Request

--- wsuwp-ucomm-asset-request/README.md
+++
@@ -1,3 +0,0 @@
-# WSUWP UComm Asset Request
-
-A WordPress plugin to provide asset request functionality to ucomm.wsu.edu
--- wsuwp-ucomm-asset-request/js/admin-asset-request.js
+++
@@ -1,8 +0,0 @@
-(function($, window){
-	// When publishing, this will flash briefly. It should say 'Approve'.
-	window.postL10n.publish = 'Approve';
-
-	// Rename the 'Publish' action to 'Approve' when applicable.
-	$( '#publish' ).attr( 'value', window.postL10n.publish );
-	$( '#original_publish' ).attr( 'value', window.postL10n.publish );
-}(jQuery, window));
