3679fdec27cc39ba78b1c04b6c867798e2d77726
Jeremy Felt <jeremy.felt@gmail.com>
2016-07-06 18:24:23 +0200

    Update BuddyPress 2.6.1.1

--- buddypress/bp-friends/bp-friends-notifications.php
+++ buddypress/bp-friends/bp-friends-notifications.php
@@ -59,9 +59,7 @@ add_action( 'friends_friendship_requested', 'friends_notification_new_request', 
  * @param int $friend_id     ID of the request recipient.
  */
 function friends_notification_accepted_request( $friendship_id, $initiator_id, $friend_id ) {
-	$initiator_id = (int) $initiator_id;
-
-	if ( 'no' == bp_get_user_meta( $initiator_id, 'notification_friends_friendship_accepted', true ) ) {
+	if ( 'no' == bp_get_user_meta( (int) $initiator_id, 'notification_friends_friendship_accepted', true ) ) {
 		return;
 	}
 
