19ad8d6039a37df5ea31406d588cd9e20fa08959
Jeremy Felt <jeremy.felt@gmail.com>
2016-07-06 18:23:47 +0200

    Update WordPress SEO 3.3.4

--- wordpress-seo/vendor/autoload.php
+++ wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitaf83f3aa46cfb916503de5a1d036c9a0::getLoader();
+return ComposerAutoloaderInitf20029986052e53fa9826780b2029c06::getLoader();
--- wordpress-seo/vendor/autoload_52.php
+++ wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit049f153404cba1ae6fdd1d01097ce970::getLoader();
+return ComposerAutoloaderInit0b36b471ac303c879fed046d17914a59::getLoader();
