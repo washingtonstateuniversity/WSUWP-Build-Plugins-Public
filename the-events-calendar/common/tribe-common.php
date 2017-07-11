92be1255384d1666ab9934c291c9a1b5950e9def
Jeremy Felt <jeremy.felt@gmail.com>
2017-07-11 13:16:54 -0700

    Update The Events Calendar 4.5.7

--- the-events-calendar/common/tribe-common.php
+++ the-events-calendar/common/tribe-common.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Description: An event settings framework for managing shared options
-Version: 4.5.5
+Version: 4.5.7
 Author: Modern Tribe, Inc.
 Author URI: http://m.tri.be/1x
 Text Domain: tribe-common
