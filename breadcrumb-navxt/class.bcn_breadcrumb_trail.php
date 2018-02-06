6cd7d98281d4969490fb3689f54ef3a9ddaea71b
Jeremy Felt <jeremy.felt@gmail.com>
2018-02-06 13:04:20 -0800

    Update Breadcrumb NavXT 6.0.4

--- breadcrumb-navxt/class.bcn_breadcrumb_trail.php
+++ breadcrumb-navxt/class.bcn_breadcrumb_trail.php
@@ -21,7 +21,7 @@ require_once(dirname(__FILE__) . '/includes/block_direct_access.php');
 class bcn_breadcrumb_trail
 {
 	//Our member variables
-	const version = '6.0.3';
+	const version = '6.0.4';
 	//An array of breadcrumbs
 	public $breadcrumbs = array();
 	public $trail = array();
@@ -91,6 +91,11 @@ class bcn_breadcrumb_trail
 			//What hierarchy should be shown leading to the post, tag or category
 			'Spost_post_hierarchy_type' => 'category',
 			//Attachment settings
+			'bpost_attachment_hierarchy_display' => true,
+			//What hierarchy should be shown leading to the attachment
+			'Spost_attachment_hierarchy_type' => 'BCN_POST_PARENT',
+			//Give an invlaid page ID for the attachement root
+			'apost_attachment_root' => 0,
 			//The breadcrumb template for attachment breadcrumbs
 			'Hpost_attachment_template' => bcn_breadcrumb::get_default_template(),
 			//The breadcrumb template for attachment breadcrumbs, used when an anchor is not needed
@@ -1083,11 +1088,15 @@ class bcn_breadcrumb_trail
 				$this->type_archive($type);
 				$type_str = $this->get_type_string_query_var($wp_taxonomies[$type->taxonomy]->object_type[0]);
 			}
-			else if($this->opt['bblog_display'])
+			//Otherwise, it's likely the blog page
+			else if($this->opt['bblog_display'] || is_home())
 			{
 				$type_str = 'post';
 			}
-			$this->do_root($type_str, $this->opt['apost_' . $type_str . '_root'], is_paged(), $this->treat_as_root_page($type_str));
+			if(isset($this->opt['apost_' . $type_str . '_root']))
+			{
+				$this->do_root($type_str, $this->opt['apost_' . $type_str . '_root'], is_paged(), $this->treat_as_root_page($type_str));
+			}
 		}
 		//We always do the home link last, unless on the frontpage
 		if(!is_front_page())
