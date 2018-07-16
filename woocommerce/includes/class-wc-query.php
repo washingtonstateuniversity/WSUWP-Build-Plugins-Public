55d63b2d52ab022a3000c38c8b6d53f7bd67a139
Jeremy Felt <jeremy.felt@gmail.com>
2018-07-16 16:13:29 -0700

    Update WooCommerce 3.4.3

--- woocommerce/includes/class-wc-query.php
+++ woocommerce/includes/class-wc-query.php
@@ -176,8 +176,6 @@ class WC_Query {
 	/**
 	 * Add query vars.
 	 *
-	 * @access public
-	 *
 	 * @param array $vars Query vars.
 	 * @return array
 	 */
@@ -371,7 +369,6 @@ class WC_Query {
 	 *
 	 * Hooked into wpseo_ hook already, so no need for function_exist.
 	 *
-	 * @access public
 	 * @return string
 	 */
 	public function wpseo_metakey() {
@@ -517,9 +514,9 @@ class WC_Query {
 		if ( isset( $wp_query->queried_object, $wp_query->queried_object->term_taxonomy_id, $wp_query->queried_object->taxonomy ) && is_a( $wp_query->queried_object, 'WP_Term' ) ) {
 			$search_within_terms = get_terms( array(
 				'taxonomy' => $wp_query->queried_object->taxonomy,
-				'parent' => $wp_query->queried_object->term_id,
-				'fields' => 'tt_ids',
-			));
+				'child_of' => $wp_query->queried_object->term_id,
+				'fields'   => 'tt_ids',
+			) );
 			$search_within_terms[] = $wp_query->queried_object->term_taxonomy_id;
 			$args['join']         .= " INNER JOIN (
 				SELECT post_id, min( meta_value+0 ) price
@@ -550,9 +547,9 @@ class WC_Query {
 		if ( isset( $wp_query->queried_object, $wp_query->queried_object->term_taxonomy_id, $wp_query->queried_object->taxonomy ) && is_a( $wp_query->queried_object, 'WP_Term' ) ) {
 			$search_within_terms = get_terms( array(
 				'taxonomy' => $wp_query->queried_object->taxonomy,
-				'parent' => $wp_query->queried_object->term_id,
-				'fields' => 'tt_ids',
-			));
+				'child_of' => $wp_query->queried_object->term_id,
+				'fields'   => 'tt_ids',
+			) );
 			$search_within_terms[] = $wp_query->queried_object->term_taxonomy_id;
 			$args['join']         .= " INNER JOIN (
 				SELECT post_id, max( meta_value+0 ) price
