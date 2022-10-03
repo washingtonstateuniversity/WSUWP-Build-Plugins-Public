38fc06cec042387f0ef13da4a6f6aa6ecb30af29
Danial Bleile <danbleile@gmail.com>
2022-10-03 11:48:47 -0700

    Hotfix | Allow admins to edit settings

--- google-sitemap-generator/sitemap.php
+++ google-sitemap-generator/sitemap.php
@@ -16,7 +16,7 @@
  Plugin Name: Google XML Sitemaps
  Plugin URI: http://www.arnebrachhold.de/redir/sitemap-home/
  Description: This plugin improves SEO using sitemaps for best indexation by search engines like Google, Bing, Yahoo and others.
- Version: 4.1.1
+ Version: 4.1.1.1
  Author: Auctollo
  Author URI: http://www.arnebrachhold.de/
  Text Domain: sitemap
